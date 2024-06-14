bacc44fcf3cdfc603dce09ed793d97b9970c786b
Yifan Xiong <53613810+EwanXiong@users.noreply.github.com>
2024-06-14 17:26:17 +0800

    Create Task3.py

---
+++ Task3.py
@@ -0,0 +1,22 @@
+"""
+Problem:
+Given a string s, please find the longest sub string that does not contain repeated characters
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        length = 0
+        
+        queue = []
+        for i in s:
+            if i in queue:
+                length = max(length, len(queue))
+                while i in queue:
+                    queue.pop(0)
+
+            queue.append(i)
+
+        return max(length, len(queue))
+
+
