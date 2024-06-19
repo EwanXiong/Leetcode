178dd290c9e6dd299b13c2b62e4ad693e25f86da
Yifan Xiong <53613810+EwanXiong@users.noreply.github.com>
2024-06-19 15:41:47 +0800

    Create Task4.py

---
+++ Task4.py
@@ -0,0 +1,8 @@
+"""
+Problem:
+Given an integer x, if x is a palindrome, return true; otherwise, return false. 
+Palindrome number it means that the integers are read the same in forward order (from left to right) and reverse order (from right to left).
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
