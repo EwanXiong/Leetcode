2464977e7bbbe12e641104fc3338f63242376db3
Yifan Xiong <53613810+EwanXiong@users.noreply.github.com>
2024-06-19 16:06:05 +0800

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -6,3 +6,14 @@ Palindrome number it means that the integers are read the same in forward order 
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+      input_int = str(x)
+      p_int = input_int[::-1]
+      if input_int == p_int:
+        print("The number ", x, " is a Palindrome")
+        return(True)
+      else:
+        print("The number ", x, " is not a Palindrome")
+        return(False)
+
+S = Solution()
+S.isPalindrome('121')
