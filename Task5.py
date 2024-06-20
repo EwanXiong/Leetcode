60f8d2ec2e0efd1a8e520eb71fc1cc3f7ba7756c
Yifan Xiong <53613810+EwanXiong@users.noreply.github.com>
2024-06-20 10:40:56 +0800

    Create Task5.py

---
+++ Task5.py
@@ -0,0 +1,36 @@
+"""
+Problem:
+Given a Roman number, convert it to an integer.
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+"""
+
+class Solution:
+    SYMBOL_VALUES = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        n = len(s)
+        for i, ch in enumerate(s):
+            value = Solution.SYMBOL_VALUES[ch]
+            if i < n - 1 and value < Solution.SYMBOL_VALUES[s[i + 1]]:
+                ans -= value
+            else:
+                ans += value
+        return ans
+
+solution = Solution()
+solution.romanToInt('XXVII')
